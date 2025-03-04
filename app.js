70736c0c8f819ce408f419cb2f5f55cbf883d4f4
kworster <kworster@users.noreply.github.com>
2025-03-04 07:07:55 +0000

    deploy: 37f27edf32a05080ede006ce9cf3851b13cd1af6

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const sw = new URL('service-worker.js', import.meta.url)
 if ('serviceWorker' in navigator) {
     const s = navigator.serviceWorker;
     s.register(sw.href, {
-        scope: '/CheckList/'
+        scope: '/tests/'
     })
         .then(_ => console.log('Service Worker Registered for scope:', sw.href, 'with', import.meta.url))
         .catch(err => console.error('Service Worker Error:', err));
