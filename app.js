4a8a794cda5cb9d4ef9e9bffcc07411201870c7c
kworster <kworster@users.noreply.github.com>
2025-03-04 06:45:44 +0000

    deploy: b2df23f88cd5daa1ffc9f13961854815c10dc0d0

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// const analytics = getAnalytics(app);
 
 const taskInput = document.getElementById('taskInput');
 const addTaskBtn = document.getElementById('addTaskBtn');
